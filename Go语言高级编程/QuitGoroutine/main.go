7d00f25b5b47e59d5db48c886de8db77ee11d917
mcc <ma.cc@qq.com>
2019-11-27 15:13:44 +0800

    安全退出工作Goroutine

--- Go语言高级编程/QuitGoroutine/main.go
+++ Go语言高级编程/QuitGoroutine/main.go
@@ -1,1 +1,35 @@
-package QuitGoroutine
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func worker(wg *sync.WaitGroup, cannel chan struct{}) {
+	defer wg.Done()
+
+	for {
+		select {
+		default:
+			fmt.Println("hello")
+		case <-cannel:
+			return
+		}
+	}
+}
+
+func main() {
+	cancel := make(chan struct{})
+
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go worker(&wg, cancel)
+	}
+	time.Sleep(time.Second)
+	close(cancel)
+	wg.Wait()
+}
+
+// 通过关闭channel会使所有读取它的收到一个空数据结合select和WaitGroup安全关闭工作Goroutine.
